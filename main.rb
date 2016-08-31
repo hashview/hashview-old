4fe0f3a466663f00f187a017cbede11f461b902a
i128 <jarsnah12@gmail.com>
2016-08-30 20:39:56 -0500

    Added more conditional checking before import

--- main.rb
+++ main.rb
@@ -478,10 +478,12 @@ post '/job/:id/upload/verify_hashtype' do
 
   filetype = params[:filetype]
   hash = params[:hash]
-  if params[:hashtype] == '99999'
+  if params[:hashtype] == '99999' && !params[:manualHash].nil?
     hashtype = params[:manualHash]
-  else
+  elsif params[:hashtype] != '99999' && params[:manualHash].nil?
     hashtype = params[:hashtype]
+  else
+    return 'Invalid hashtype detected'
   end
 
   hash_file = "control/hashes/hashfile_upload_jobid-#{params[:id]}-#{params[:hash]}.txt"
