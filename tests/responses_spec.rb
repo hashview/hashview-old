636a908290a3a502e6f2524eea30eea99ca79c90
ccammilleri <caseycam@gmail.com>
2016-09-11 22:32:59 -0500

    change purge behvior

--- tests/responses_spec.rb
+++ tests/responses_spec.rb
@@ -62,7 +62,7 @@ class MyTest < MiniTest::Test
   def test_authd_404_response
     userid = login_testuser
     get '/thisshouldneverexist'
-    assert_equal last_response.status, 404
+    assert_equal 404, last_response.status
     delete_testuser(userid)
   end
 
@@ -217,6 +217,13 @@ class MyTest < MiniTest::Test
     userid = login_testuser
     get '/purge'
     assert_equal 200, last_response.status
+    delete_testuser(userid)
+  end
+
+  def test_purge_post_response
+    userid = login_testuser
+    post '/purge'
+    assert_equal 302, last_response.status
     delete_testuser(userid)
   end
 
