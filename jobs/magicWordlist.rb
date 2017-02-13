ebb3413ca05531d2e2c6fde0f9afd4038d050f59
i128 <jarsnah12@gmail.com>
2017-02-12 19:16:54 -0600

    Added support to include cracked plaintexts to magic wordlist.

--- jobs/magicWordlist.rb
+++ jobs/magicWordlist.rb
@@ -1,7 +1,17 @@
 def generateMagicWordlist
+  # Get list of all plaintext passwords and save it to a file
+  @plaintexts = Hashes.all(fields: [:plaintext], cracked: 1, unique: true, order: [:plaintext.asc])
+  file_name = '/tmp/plaintext.txt'
+
+  File.open(file_name, 'w') do |f|
+    @plaintexts.each do |entry|
+      f.puts entry.plaintext
+    end
+  end
+   
   # Get list of all wordlists
   # TODO add --parallel #
-  shell_cmd = 'sort -u '
+  shell_cmd = 'sort -u /tmp/plaintext.txt '
   @wordlists = Wordlists.all
   @wordlists.each do |entry|
     shell_cmd = shell_cmd + entry.path.to_s + ' '
@@ -12,6 +22,9 @@ def generateMagicWordlist
 
   shell_mv_cmd = 'mv /tmp/MagicWordlist.txt control/wordlists/MagicWordList.txt'
   system(shell_mv_cmd)
+
+  # Remove plaintext list
+  File.delete('/tmp/plaintext.txt')
 end
 
 class MagicWordlist
@@ -25,7 +38,17 @@ class MagicWordlist
       p 'LAST UPDATED: ' + has_magic_wordlist.lastupdated.to_s
       # Has magic wordlist already, need to check if its outdated
       @wordlists = Wordlists.all(:lastupdated.gt => has_magic_wordlist.lastupdated)
-      if @wordlists.size > 0
+      @plaintexts = Hashes.all(:lastupdated.gt => hash_magic_wordlist.lastupdated)
+      if @wordlists.size > 0 || @plaintexts.size > 0
+
+        file_name = '/tmp/plaintext.txt'
+
+        File.open(file_name, 'w') do |f|
+          @plaintexts.each do |entry|
+            f.puts entry.plaintext
+          end
+        end
+
         p 'Our magic wordlist is out of date'
         shell_cmd = 'sort -u control/wordlists/MagicWordList.txt '
         @wordlists.each do |entry|
@@ -47,6 +70,9 @@ class MagicWordlist
         has_magic_wordlist.lastupdated = Time.now
         has_magic_wordlist.size = size
         has_magic_wordlist.save       
+
+        # Remove plaintext list
+        File.delete('/tmp/plaintext.txt')
 
       else
         p 'Our magic wordlist is current.'
