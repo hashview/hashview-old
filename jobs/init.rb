d3b9919848d27f8cb01692616c9005a516f88d27
ccammilleri <caseycam@gmail.com>
2017-02-20 16:40:05 -0600

    add test job, remove old jobq

--- jobs/init.rb
+++ jobs/init.rb
@@ -2,5 +2,5 @@
 require_relative 'management'
 require_relative 'wordlistImporter'
 require_relative 'magicWordlist'
-require_relative 'jobq'
 require_relative 'background_worker'
+require_relative 'test_job'
