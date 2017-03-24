4ee3db615ef43b988243343f0529cb334002ec2b
i128 <jarsnah12@gmail.com>
2017-03-23 21:14:02 -0500

    code cleanup

--- routes/customers.rb
+++ routes/customers.rb
@@ -163,9 +163,6 @@ end
 get '/customers/upload/verify_filetype' do
   varWash(params)
 
-  hashfile = Hashfiles.first(id: params[:hashid])
-
-  #@filetypes = detectHashfileType("control/hashes/hashfile_upload_job_id-#{params[:job_id]}-#{hashfile.hash_str}.txt")
   @filetypes = %w(pwdump shadow dsusers smart_hashdump $hash $user:$hash $hash:$salt)
   @job = Jobs.first(id: params[:job_id])
   haml :verify_filetypes
