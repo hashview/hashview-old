414afb97d59e027cd0fe94a79538c85369686a84
ccammilleri <caseycam@gmail.com>
2017-05-20 15:02:13 -0500

    remove ui warning now that config option is moved from db to file

--- hashview.rb
+++ hashview.rb
@@ -40,11 +40,6 @@ before /^(?!\/(login|register|logout|v1\/))/ do
   @settings = Settings.first
   if !validSession?
     redirect to('/login')
-  else
-    hc_settings = HashcatSettings.first
-    if (hc_settings && hc_settings.hc_binpath.nil?) || hc_settings.nil?
-      flash[:warning] = 'Annoying alert! You need to define hashcat\'s binary path in settings first. Do so <a href=/settings>HERE</a>'
-    end
   end
 end
 
