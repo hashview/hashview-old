5ab4a7f77563e01aec289da10f173bb10671674a
i128 <jarsnah12@gmail.com>
2017-12-01 14:59:08 -0600

    Test user delete

--- models/master.rb
+++ models/master.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sequel'
 require 'bcrypt'
 require 'rotp'
+require 'logger'
 
 Sequel::Model.plugin :json_serializer
 
@@ -11,15 +12,15 @@ options = YAML.load_file('config/database.yml')
 # there has to be a better way to handle this shit
 if ENV['RACK_ENV'] == 'test'
   HVDB = Sequel.mysql(options['test'])
-  #HVDB.loggers << Loggers.new(STDOUT)
+  HVDB.loggers << Loggers.new(STDOUT)
   HVDB.sql_log_level = :debug
 elsif ENV['RACK_ENV'] == 'development'
   HVDB = Sequel.mysql(options['development'])
-  #HVDB.loggers << Logger.new(STDOUT)
+  HVDB.loggers << Logger.new(STDOUT)
   HVDB.sql_log_level = :debug
 elsif ENV['RACK_ENV'] == ('production' || 'default')
   HVDB = Sequel.mysql(options['production'])
-  #HVDB.loggers << Logger.new(STDOUT)
+  HVDB.loggers << Logger.new(STDOUT)
 else
   puts 'ERROR: You must define an environment. ex: RACK_ENV=production'
   exit
