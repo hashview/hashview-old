4a8a9f7811f444d0745fc8e4aa1c8e02814e728b
i128 <jarsnah12@gmail.com>
2018-02-19 20:10:38 -0600

    Added Dynamic Wordlist All

--- helpers/dynamicWordlists.rb
+++ helpers/dynamicWordlists.rb
@@ -3,14 +3,14 @@ def updateDynamicWordlist(wordlist_id)
   wordlist = Wordlists.first(id: wordlist_id)
   file = wordlist.path
 
-  hashfile = Hashfiles.first(wl_id: wordlist_id)
-  if !hashfile.nil?
-    @results = HVDB.fetch('SELECT h.plaintext FROM hashes h LEFT JOIN hashfilehashes a ON h.id = a.hash_id LEFT JOIN hashfiles f on a.hashfile_id = f.id WHERE (a.hashfile_id = ? and h.cracked = 1)', hashfile[:id])
-  else
-    # Maybe we have a customer list?
-    # BTW this is dumb logic, we should build a scope into the schema
+  if wordlist.scope == 'all'
+    @results = HVDB.fetch('SELECT plaintext FROM hashes WHERE cracked = 1')
+  elsif wordlist.scope == 'customer'
     customer = Customers.first(wl_id: wordlist_id)
     @results = HVDB.fetch('SELECT h.plaintext FROM hashes h LEFT JOIN hashfilehashes a on h.id = a.hash_id LEFT JOIN hashfiles f on a.hashfile_id = f.id WHERE (f.customer_id = ? AND h.cracked = 1)', customer[:id])
+  elsif wordlist.scope == 'hashfile'
+    hashfile = Hashfiles.first(wl_id: wordlist_id)
+    @results = HVDB.fetch('SELECT h.plaintext FROM hashes h LEFT JOIN hashfilehashes a ON h.id = a.hash_id LEFT JOIN hashfiles f on a.hashfile_id = f.id WHERE (a.hashfile_id = ? and h.cracked = 1)', hashfile[:id])
   end
 
   # if file is not there, create it
